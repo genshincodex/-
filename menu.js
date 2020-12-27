b33ccc07c4ce4454ede969ca219fdb03af871e02
genshincodex <76632575+genshincodex@users.noreply.github.com>
2020-12-27 17:45:30 +0800

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,15 @@
+$('#nav-artifacts').on('click', function () {
+  window.location = 'artifacts.htm';
+});
+
+$('#nav-characters').on('click', function () {
+  window.location = 'characters.htm';
+});
+
+$('#nav-domains').on('click', function () {
+  window.location = 'domains.htm';
+});
+
+$('#nav-logo').on('click', function () {
+  window.location = 'index.htm';
+});
